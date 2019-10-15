ace09fc0383b6b6de90f62e699cefa92b047a03f
Carlos Robles <dbamastery@outlook.com>
2019-10-15 11:28:29 -0600

    Delete entry_point.sh

--- SQL Saturday - Denver 2019/Demo_02/Demo_02/DBA/entry_point.sh
+++
@@ -1,3 +0,0 @@
-# Start SQL Server, then start the SQL script to restore DB and create objects
-# The wait time depends on the environment
-/db_scripts/DBA/sql_deployment.sh 20 & /opt/mssql/bin/sqlservr
